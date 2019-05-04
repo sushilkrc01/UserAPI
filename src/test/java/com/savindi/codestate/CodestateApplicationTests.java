075fcb54918e40317ae955991db7a112afd9d59c
Savindi Wijenayaka <savindi.wijenayaka@gmail.com>
2019-05-04 11:15:55 +0530

    Initial Commit

---
+++ src/test/java/com/savindi/codestate/CodestateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.savindi.codestate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodestateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
